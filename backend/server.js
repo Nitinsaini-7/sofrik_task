3b651c100cd0e45489b56e4162d67303d1f09dad
Nitinsaini-7 <saini00nitin@gmail.com>
2025-09-29 10:48:28 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
